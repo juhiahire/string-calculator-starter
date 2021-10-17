4380cf4a0ba2fdfa0a0255f410cea5a8aca2b349
juhiahire <juhiahire37@gmail.com>
2021-10-17 22:35:18 +0530

    test3

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,7 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
-   @Test public void return_0_when_input_is_empty() { assertEquals("0",
-	 given("")); }
+  /* @Test public void return_0_when_input_is_empty() { assertEquals("0",
+	* given("")); }
+	 */
+
+ 
+	// @Test public void return_3_when_input_is_1_2() { assertEquals("3",
+	// given("1,2")); }
 	 
+	 @Test public void sum_floats_and_return_float() { assertEquals("6.6",
+			 given("2.2,4.4")); }
+			
+
 }
