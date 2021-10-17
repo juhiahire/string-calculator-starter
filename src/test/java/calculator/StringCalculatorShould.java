e6aae6b46b24ae43a9e6513ba5536549d9e87f18
juhiahire <juhiahire37@gmail.com>
2021-10-17 22:37:19 +0530

    test5

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,7 +17,11 @@ class StringCalculatorShould {
 	// @Test public void sum_floats_and_return_float() { assertEquals("6.6",
 	// given("2.2,4.4")); }
 
-	 @Test public void treat_newLine_as_a_delimiter() { assertEquals("6",
-		 given("1\n2,3")); }			
+	// @Test public void treat_newLine_as_a_delimiter() { assertEquals("6",
+	// given("1\n2,3")); }	
+
+	  @Test public void return_error_msg_when_newLine_at_invalid_position() {
+			  assertEquals("Number expected but '\n' found at position 6.",
+			  given("1,2,5,\n3")); }		
 
 }
